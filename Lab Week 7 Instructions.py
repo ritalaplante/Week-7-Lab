c13f4289119d241cd7b71604f6345ee2ac32e24d
ritalaplante <45489428+ritalaplante@users.noreply.github.com>
2021-03-08 12:04:34 -0500

    Add files via upload

---
+++ Lab Week 7 Instructions.py
@@ -0,0 +1,16 @@
+#Find an intro to lists here
+#https://www.w3schools.com/python/python_lists.asp
+
+#Read in a file, split() it on space to get a list of words, then find every word
+#that starts with a capital letter or has a digit
+#See if you can read the words into a list using a list comprehension
+
+#Start with an empty list and then use a for loop to add every integer between 1
+#and 20 that is divisible by 3
+
+
+#Use a list comprehension instead of a loop to add every integer between 1 and 20
+#that is divisible by 3 to a list
+
+
+
